35aa7edbc71463da96b0e17970fa5eab75a8bfcb
dslyecix <31083701+dslyecix@users.noreply.github.com>
2018-03-24 21:42:50 -0400

    First Commit
    Path working, Basic unit pathfinding

---
+++ Assets/Path.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+    private List<Transform> waypoints;
+
+	void Start ()
+    {
+        SetWaypoints();
+        Debug.Log(waypoints.ToString());
+    }
+
+
+    void Update () {
+		for (int i = 0; i < waypoints.Count-1; i++)
+        {
+            Debug.DrawLine(waypoints[i].position, waypoints[i+1].position,Color.red);
+        }
+	}
+
+    private void SetWaypoints()
+    {
+        waypoints = new List<Transform>();
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Transform child = this.transform.GetChild(i);
+            if (child.GetComponent(typeof(Waypoint)) != null)
+                waypoints.Add(child);
+        }
+    }
+
+    public Transform GetWaypoint (int index) {
+        if (index < waypoints.Count)
+            return waypoints[index];
+        else {
+            Debug.Log("Error, no waypoints in list!");
+            return null;
+        }
+    }
+
+}
---
+++ Assets/UnitSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour {
+
+    public List<GameObject> unitList;
+    public GameObject selectedUnit;
+    public Path selectedPath;
+
+	void Start () {
+		selectedUnit = unitList[0];
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+            SpawnUnit();
+        }
+	}
+
+    private void SpawnUnit()
+    {
+        GameObject unitGO = Instantiate(selectedUnit,this.transform.position, Quaternion.identity);
+        Unit unit = unitGO.GetComponent<Unit>();
+        unit.SetCurrentPath(selectedPath);
+        unit.isActive = true;
+
+    }
+}
